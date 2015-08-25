f3e035b228a3b5bfa664b116ced24a4dfc2b00ab
MonsieurAmarillo <andy.lachkrampf@googlemail.com>
2015-08-25 22:13:05 +0200

    - changed picture size - contact.php form is ready to send

--- contact.php
+++ contact.php
@@ -1,7 +1,23 @@
 <?php
 
-echo $_POST["name"] . "\n";
+/*echo $_POST["name"] . "\n";
 echo $_POST["email"] . "\n"; 
 echo $_POST["telephone"] . "\n";
-echo $_POST["message"];
+echo $_POST["message"];*/
+
+
+ $mailtext = "";
+ $tab = ":\t";
+
+  foreach ($_POST as $name => $value) {
+         if (is_array($value)) {
+                 foreach ($value as $singleValue) {
+                 $mailtext .= $name.$tab.$singleValue."\n";
+             }
+         } else {
+             $mailtext .= $name.$tab.$value."\n";
+         }
+     }
+ echo $mailtext;     
+
 ?> 
